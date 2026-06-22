588b94202f38fbd4207ab079781167a8631763af
v-i-n-h <164168071+v-i-n-h@users.noreply.github.com>
2026-06-22 15:58:24 +0700

    update GU5/Đăng kí tài khoản bằng Google ,FB/add local cacert for google login auth

--- config/services.php
+++ config/services.php
@@ -39,6 +39,9 @@ return [
         'client_id' => env('GOOGLE_CLIENT_ID'),
         'client_secret' => env('GOOGLE_CLIENT_SECRET'),
         'redirect' => env('GOOGLE_REDIRECT_URI', env('APP_URL') . '/auth/google/callback'),
+        'guzzle' => [
+            'verify' => storage_path('certs/cacert.pem'),
+        ],
     ],
 
     'facebook' => [
